41ff668aebcef8f44133a2cf8d98e2abaab84133
Ruslan <ruslan.fekhretdinov@gmail.com>
2016-02-14 23:44:34 +0300

    add test for capitlz later

--- hero_spec.rb
+++ hero_spec.rb
@@ -2,5 +2,11 @@
 require './hero'
 
 describe Hero do
-  
+
+  it "has a capitalized name" do
+    hero = Hero.new 'mike'
+    #ожидаем что имя равно Маик hero.name == 'Mike'
+    expect(hero.name).to eq 'Mike'
+  end
+
 end
