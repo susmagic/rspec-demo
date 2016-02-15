2d3c6e4475a27e34a9be80f4fd0fd83bad7796f7
Ruslan <ruslan.fekhretdinov@gmail.com>
2016-02-15 22:53:44 +0300

    add test for car

--- car.rb
+++ car.rb
@@ -17,7 +17,3 @@ class Car
 		0
 	end
 end
-
-car = Car.new
-car.add_fuel 10
-puts car.range
---
+++ car_spec.rb
@@ -0,0 +1,11 @@
+require './car'
+
+describe Car do
+
+	it "must return range" do
+		car = Car.new
+		car.add_fuel 10
+		expect(car.range).to eq 200 
+	end
+
+end
